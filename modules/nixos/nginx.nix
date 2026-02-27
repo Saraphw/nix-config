1dfab2f07aa717d83433291c4f272258607862bd
Saraphw <sarahpweddle@gmail.com>
2026-02-26 20:16:10 -0500

    Updated nginx

--- modules/nixos/nginx.nix
+++ modules/nixos/nginx.nix
@@ -1,13 +1,16 @@
-{pkgs, ...}:{
+{pkgs, ...}:
+let website = pkgs.fetchFromGitHub {
+  owner = "Saraphw";
+  repo = "WWWSeraph";
+  rev = "7c1c482ed0b955b164fab244e672ee3d48b5ce46";
+  sha256 = "sha256-qd6T/ZynQroIsFUrz/v1cbCaZBQqVl8dbLSgRQd6R+g=";
+};
+in
+{
   services.nginx = {
     enable = true;
     virtualHosts.localhost = {
-      locations."/" = {
-        return = "200 <html><body>Hello World</body></html>";
-        extraConfig = ''
-          default_type text/html;
-        '';
-      };
+      root = website;
     };
   };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
